1ab0dbb7d098026536e9552f5b771a3a0881b886
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-09-16 23:00:44 -0300

    Criei o DatabaseConstructorTesting, que é responsável por criar o contexto no banco de dados para ser usados em testes. Ele usa um banco de dados local.
    Além disso, já foi criado o método para criar o contexto de UserContext

---
+++ ZyronTests/DatabaseConstructorTesting.cs
@@ -0,0 +1,43 @@
+﻿using ZyronChatWebApp;
+using Microsoft.EntityFrameworkCore;
+using ZyronChatWebApp.Data;
+
+public class DatabaseConstructorTesting
+{
+    private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=EFTestSample;Trusted_Connection=True";
+    private const string ConnectionStringMvcIndependentObjectsOfProductsContext = @"Server=(localdb)\mssqllocaldb;Database=TestMvcIndependentObjectsOfProductsContext;Trusted_Connection=True";
+    private static readonly object _lock = new();
+    private static bool _databaseInitialized;
+
+    public DatabaseConstructorTesting()
+    {
+        lock (_lock)
+        {
+            if (!_databaseInitialized)
+            {
+                using (var context = CreateContext())
+                {
+                    context.Database.EnsureDeleted();
+                    context.Database.EnsureCreated();
+
+
+                }
+
+                _databaseInitialized = true;
+
+
+            }
+        }
+    }
+
+    public UserContext CreateContext()
+    {
+        return new UserContext(new DbContextOptionsBuilder<UserContext>()
+                .UseSqlServer(ConnectionString)
+                .Options);
+
+    }
+
+
+
+}
