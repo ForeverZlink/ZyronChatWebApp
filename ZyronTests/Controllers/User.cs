c18d46f123e4495c02c862471baf16d724cd5892
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-09-16 22:38:50 -0300

    Criei o projeto ZyronTest. Esse projeto terá todos os testes relacionados a aplicação ZyronChatWebApp
    Para já começar:
    -Foi criado um diretório para testes de controllers.
    -Criado um arquivo de testes para o controller User.  Além do corpo do teste para o metódo RegisterUser.
    -Como será necessário usar arquivos de classe do projeto ZyronChatWebApp, já adicionei uma referência para que eu consiga acessar no projeto de testes.

---
+++ ZyronTests/Controllers/User.cs
@@ -0,0 +1,14 @@
+using ZyronChatWebApp.Controllers;
+using ZyronChatWebApp.Models;
+
+namespace ZyronTests.Controllers
+{
+    public class User
+    {
+        [Fact]
+        public void RegisterUser()
+        {
+
+        }
+    }
+}
