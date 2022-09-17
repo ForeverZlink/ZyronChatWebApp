80b8a9700428124c8d1a209b3570b46317fc13d1
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-09-16 22:01:52 -0300

    Criei a base estrutural do context para o model user.

---
+++ ZyronChatWebApp/Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ZyronChatWebApp.Models;
+
+
+namespace ZyronChatWebApp.Data
+{
+    public class UserContext:DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options):base(options)
+        { }
+        
+        public DbSet<ZyronChatWebApp.Models.User> Users { get; set; }
+    }
+
+}
