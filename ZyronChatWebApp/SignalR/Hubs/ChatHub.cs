fdd1af34830855fa80777e674713bc87ae405fba
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-09-16 21:32:28 -0300

    Criei um método que envia messagens para outro usuário

--- ZyronChatWebApp/SignalR/Hubs/ChatHub.cs
+++ ZyronChatWebApp/SignalR/Hubs/ChatHub.cs
@@ -4,5 +4,12 @@ namespace ZyronChatWebApp.SignalR.Hubs
 {
     public class ChatHub:Hub
     {
+        public  Task SendMessagePrivate(string user, string message)
+        {
+            //Send a private message for a other user. 1x1 chat
+            
+            return  this.Clients.User(user).SendAsync("ReceiveMessage", message);
+           
+        }
     }
 }
