63b1e06e668e756b6b09ad5e0fb979f3068e76f8
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-09-21 20:13:40 -0300

    Criei um novo model que herda da classe identityUser, pois assim eu posso adicionar mais campos conforme a demanda.
    Também criei as migrations para as alterações

--- ZyronChatWebApp/Migrations/20220921003056_NewStartingModelUser.Designer.cs
+++ ZyronChatWebApp/Migrations/20220921003056_NewStartingModelUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZyronChatWebApp.Data;
 
@@ -11,9 +12,10 @@ using ZyronChatWebApp.Data;
 namespace ZyronChat.Data.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextContextModelSnapshot : ModelSnapshot
+    [Migration("20220921003056_NewStartingModelUser")]
+    partial class NewStartingModelUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
