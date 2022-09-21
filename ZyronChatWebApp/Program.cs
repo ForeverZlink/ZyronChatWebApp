eeee30b63b64f63162a5bb3167024f29e4eb22e9
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-09-21 20:24:56 -0300

    Adicionei o entity ao programa, sendo ele relacionado ao model UserModelCustom, no contexto UserContext. Com esse código, agora posso logar usuários, criar eles e afins.  Esses usuários são armazenados no banco de dados.
    Além disso, também foi adicionado um configuração para criação de novos usuários e afins.

--- ZyronChatWebApp/Program.cs
+++ ZyronChatWebApp/Program.cs
@@ -19,9 +19,31 @@ builder.Services.AddDbContext<UserContext>(
         builder.Configuration.GetConnectionString("UserContext")
         )
     );
-builder.Services.AddIdentityCore<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity<UserModelCustom,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<UserContext>()
     ;
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+
 
 var app = builder.Build();
 
@@ -45,5 +67,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=DashManagement}/{action=Index}/{id?}");
 
-app.MapHub<ChatHub>("/Chat");
+app.MapHub<ChatHub>("/chat");
 app.Run();
