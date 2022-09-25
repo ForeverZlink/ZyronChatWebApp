cc285f251126ce6347b49478a921b10ad5070f92
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-09-25 19:26:59 -0300

    Criei meu teste para a criação de usuários.
    Primeiramente ele define algumas informações para servirem de data.
    
    Foi criado a classe IdentityResultMock, que herda a classe IdentityResult, e permite alterar a propriedade Succeeded pelo construtor, sendo que não é possível alterar o valor dessa propriedade fora da classe.
    Essa classe é necessária para quando algum método que retorne o tipo IdentityResult e precise da propriedade Succeeded para verificar se a operação foi um sucesso.
    
    Depois é instânciado um objeto Mock do tipo UserManager(para servir de injeção de depedência para o controller).
    Após isso, é definido o setup para quando CreateAsync é chamado, sendo como gatilho passar qualquer instância do tipo UserModelCustom como argumento e qualquer tipo string para o argumento password.
    Neste primeiro caso, é retornado um nova instância da classe IdentityResultMock, sendo true como padrão. Succeeded se mantém como true porque essa primeira parte do teste busca
    como resultado esperado o sucesso da criação do usuário. Assim, quando for chamado CreateAsync no método RegisterUser, o mock irá retornar IdentityResultMock com a propriedade Succeessed como true, simbolizando o sucesso da criação.
    
    Logo após, é passado isso como o depedência para o controller, e logo após o método RegisterUser é chamado.
    
    Após isso, é verificado se o resultado contém o valor contindo na propriedade SucessReturnControllerActionRedirection.
    
    ///Teste que espera Succeeded como false
    Nesse teste, é simulado a situação em que não foi possível criar um novo usuário. Assim
    segui, os mesmos passos anteriores, porém, alterei o valor da IdentityResultMock para false
    
    Após isso, criei um nova instância do controller e passei esse objeto mock como depedência.
    
    Nesse caso, era esperado que o código explorasse uma situação de falha na criação do usuário.
    
    Assim é chamado o método do controller, e é verificado se o valor FailedOfCreateUserBecauseSuccededIsFalse existe no resultado  retornado pelo controller

--- ZyronTests/Controllers/User.cs
+++ ZyronTests/Controllers/User.cs
@@ -19,9 +19,46 @@ namespace ZyronTests.Controllers
         public string SucessReturnControllerActionRedirection = "RedirectToAction";
 
         [Fact]
-        public void RegisterUser()
+        public  void RegisterUser()
         {
+            //Data to creation 
+            var UserModel = new UserModelCustom() { UserName = "carlos", Email = "carlos@gmail.com" };
+            string password = "St;111jklfald";
 
+            //Mock config to when CreateAsync is called
+            //The type of parameter of CreateAsync must be a It.IsAny<Type Of Model Required(like UserModelCustom)> 
+            //because this manners possibility that any instance of the type choised can be passed as argument and will 
+            //work. Case the type configured of CreateAsync mock been a instance, if another instance, same of type, not will work
+
+            //the return of CreateAsync in this case musb be true in Succeeded property of IdentityResult, because of this a create a 
+            //class to handle of this. Sucessed its necessary because RegisterUser action have a logic  structure  to verify the property
+
+            var mockManageUsers = new Mock<UserManager<UserModelCustom>>(Mock.Of<IUserStore<UserModelCustom>>(), null, null, null, null, null, null, null, null);
+            mockManageUsers.Setup(x => x.CreateAsync(It.IsAny<UserModelCustom>(), It.IsAny<string>())).ReturnsAsync( new IdentityResultMock());
+
+            var control = new UserController(context,null, mockManageUsers.Object);
+            var result = control.RegisterUser(UserModel.UserName,password,UserModel.Email);
+           
+            //verify if the action can got the user. True result is wanted in this test.
+            Assert.Contains(this.SucessReturnControllerActionRedirection, result.Result.ToString());
+
+            mockManageUsers.Setup(x => x.CreateAsync(It.IsAny<UserModelCustom>(), It.IsAny<string>())).ReturnsAsync(new IdentityResultMock(false));
+            control = new UserController(context, null, mockManageUsers.Object);
+
+            var FailedOfCreateUserBecauseSuccededIsFalse= control.RegisterUser(UserModel.UserName, password, UserModel.Email);
+            Assert.Contains("ViewResult", FailedOfCreateUserBecauseSuccededIsFalse.Result.ToString());
+        }
+
+    }
+
+    //Class to change Succeeded property of IdentityResult. Very useful to mock values.
+    public class IdentityResultMock : IdentityResult
+    {
+        public IdentityResultMock(bool succeeded = true)
+        {
+            this.Succeeded = succeeded;
         }
     }
+
+    
 }
