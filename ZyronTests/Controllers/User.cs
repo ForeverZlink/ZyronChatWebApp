abb1a6045a6e9ace5dd0e3266ae6185d080917c9
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-09-25 18:14:01 -0300

    Importei alguns pacotes que serão necessários futuramente no projeto

--- ZyronTests/Controllers/User.cs
+++ ZyronTests/Controllers/User.cs
@@ -1,5 +1,12 @@
+using Microsoft.AspNetCore.Identity;
 using ZyronChatWebApp.Controllers;
+using ZyronChatWebApp.Controllers.Account;
+using ZyronChatWebApp.Data;
 using ZyronChatWebApp.Models;
+using Moq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace ZyronTests.Controllers
 {
