1678773b36e15578ad3a8517aafde53a41328c2c
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-10-06 21:57:58 -0300

    Fiz algumas alterações no teste e agora ele testa apenas uma situação. Primeiramente, é definido os dados e, logo depois, já é configurado o retorno do CreateAsync do objeto mockado mockedUserManager.
    Após configurado o retorno, é passado para o campo UserManagement do controller o objeto mockado anteriormente.
    
    Após isso, é  chamado o método RegisterUser do controller e verifcado o tipo do retorno, que nesse caso é necessário o tipo ViewResult.
    
    Também é verificado se o campo UserCreatedWithSucess é true, sinalizando que o usuário foi criado com sucesso.

--- ZyronTests/Controllers/User.cs
+++ ZyronTests/Controllers/User.cs
@@ -127,10 +127,11 @@ namespace ZyronTests.Controllers
 
             Assert.IsType<ViewResult>(resultPasswordIsIncorrect);
         }
+
+        [Fact]
+        public async void RegisterUser__UserCreatedWithSucess_NoErros()
         {
-            //Data to creation 
-            var UserModel = new UserModelCustom() { UserName = "carlos", Email = "carlos@gmail.com" };
-            string password = "St;111jklfald";
+
 
             //Mock config to when CreateAsync is called
             //The type of parameter of CreateAsync must be a It.IsAny<Type Of Model Required(like UserModelCustom)> 
@@ -140,8 +141,22 @@ namespace ZyronTests.Controllers
             //the return of CreateAsync in this case musb be true in Succeeded property of IdentityResult, because of this a create a 
             //class to handle of this. Sucessed its necessary because RegisterUser action have a logic  structure  to verify the property
 
-            var mockManageUsers = new Mock<UserManager<UserModelCustom>>(Mock.Of<IUserStore<UserModelCustom>>(), null, null, null, null, null, null, null, null);
-            mockManageUsers.Setup(x => x.CreateAsync(It.IsAny<UserModelCustom>(), It.IsAny<string>())).ReturnsAsync( new IdentityResultMock());
+            var UserModel = new UserModelCustom() { UserName = "Carlosss", Email = "carlsdfdsfsos@gmail.com" };
+            string password = "St;111jklfald";
+
+            
+            mockedUserManager.Setup(x => x.CreateAsync(It.IsAny<UserModelCustom>(), It.IsAny<string>())).ReturnsAsync(new IdentityResultMock());
+            controller.UserManagement = mockedUserManager.Object;
+            var result = await controller.RegisterUser(UserModel.UserName, password, UserModel.Email);
+
+
+            //Verify if the user was created with sucess           
+            //In the controller action, when the operation of cration occurs with sucess, the action create 
+            // a view bag with true. So, this assert verify this.
+
+            var resultUserCreatedWithSucess = Assert.IsType<ViewResult>(result);
+            Assert.True( controller.ViewBag.UserCreatedWithSucess);
+        }
 
             var control = new UserController(context,null, mockManageUsers.Object);
             var result = control.RegisterUser(UserModel.UserName,password,UserModel.Email);
