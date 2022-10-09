ec9a803463616cee97f2471f60c2a1fc5c4b1b1c
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-10-08 22:55:28 -0300

    Alterei o tipo do UserId para o string, pois é necessário para a construção de relação entre UserModelCustom e UserScheduleListOfContacts

--- ZyronChatWebApp/Models/UserScheduleListOfContacts.cs
+++ ZyronChatWebApp/Models/UserScheduleListOfContacts.cs
@@ -8,7 +8,7 @@ namespace ZyronChatWebApp.Models
         [Key]
         public int Id { get; set; }
 
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public UserModelCustom User { get; set; }
 
         public ICollection<ContactInformations> ContactsInformations { get; set; }
