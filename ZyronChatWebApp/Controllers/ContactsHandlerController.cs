3861fae652a45ee68bf18c298c7722707af954d6
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-10-12 18:58:49 -0300

    Criei  o controller ContactsHandler.
    Nele, há ao método NewContact, que é o responsável por cadastrar um novo contato na lista do usuário.
    
    Primeiro o método tenta encontrar o usuário e caso não seja possível, já é retornado uma view not found.
    
    Caso seja possivel encontrar o usuário, já é buscado o objeto do tipo UserScheduleListOfContacts relationado com o id do usuário atual.
    
    Feito isso, é criado uma instância do tipo ContactInformations, usando como IdUserScheduleListOfContactso o id do objeto UserScheduleListOfContactsInstance. Assim, o objeto ContactInformations faz refêrencia a essa lista e, como cada usuário possui apenas UserScheduleListOfContacts, ContactInformations está ligado com o o usuário atráves de UserScheduleListOfContacts.
    
    Como a relação entre UserScheduleListOfContacts e ContactInformations é one to many, é possível que vários objetos do tipo ContactInformations  possam fazer referência a um único objeto do tipo UserScheduleListOfContacts

---
+++ ZyronChatWebApp/Controllers/ContactsHandlerController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Session;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Xml.Linq;
+using ZyronChatWebApp.Data;
+using ZyronChatWebApp.Models;
+
+namespace ZyronChatWebApp.Controllers
+{
+    public class ContactsHandlerController : Controller
+    {
+        public UserContext Context { get; set; }
+
+        public ContactsHandlerController(UserContext dbContext)
+        {
+            this.Context = dbContext;
+
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NewContact(string NameOfContact, string Surname)
+        {
+            var username = User.FindFirstValue(ClaimTypes.Name);
+            var user = this.Context.Users.FirstOrDefault(x => x.UserName == username);
+           
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                //Seach in database the object create when the user was created in the first time. 
+                //Here its possible find because when the user was created a new UserScheduleListOfContacts object receive a UserId
+                //This Userid its the prove of relationship among both entitys
+                //Not its possible exists two UserScheduleListOfContacts with the same UserId
+                var UserScheduleListOfContactsInstance = this.Context.UserScheduleListOfContacts.FirstOrDefault(x => x.UserId == user.Id);
+
+                var ContactInfo = new ContactInformations() { UsernameOfIdentification = NameOfContact, Surname = Surname,IdUserScheduleListOfContacts= UserScheduleListOfContactsInstance.Id};
+                
+                this.Context.Add(ContactInfo);
+                this.Context.SaveChanges();
+
+                
+                return RedirectToAction("Index","DashManagement");
+            }
+            
+
+
+
+
+        }
+    }
+}
