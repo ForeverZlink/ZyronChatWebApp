10564434843919c5dd555389cb8b90d203e6b681
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-10-13 21:37:51 -0300

    Alterei o nome do controller, pois agora fica mais claro e óbvio sua funcionalidade. Além disso, já criei o corpo do método de mostrar o chat de um usuário especifico.

---
+++ ZyronChatWebApp/Controllers/ChatMessagesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ZyronChatWebApp.Models;
+
+namespace ZyronChatWebApp.Controllers
+{
+    public class ChatMessagesController : Controller
+    {
+        private readonly ILogger<ChatMessagesController> _logger;
+
+        public ChatMessagesController(ILogger<ChatMessagesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ChatWithASpecificUser(string UserToSendAMessageIdentification)
+        {
+            return View();
+        }
+    }
+}
