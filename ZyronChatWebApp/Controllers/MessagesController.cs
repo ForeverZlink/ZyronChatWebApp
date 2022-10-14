1cc23dfa1153c55b96b07aea7ae5d7245eecc57d
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-10-13 21:30:37 -0300

    Criei um novo controller.
    Esse controller será o responsável por lidar com as mensagens e tudo que seja relacionado a isso, principalmente no quesito de armazenamento delas no banco de dados, retornos de views e afins. Lembrando que ela não irá mandar mensagems em tempo real, pois isso é função do SIGNALR e Hubs

---
+++ ZyronChatWebApp/Controllers/MessagesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ZyronChatWebApp.Models;
+
+namespace ZyronChatWebApp.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly ILogger<MessagesController> _logger;
+
+        public MessagesController(ILogger<MessagesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
