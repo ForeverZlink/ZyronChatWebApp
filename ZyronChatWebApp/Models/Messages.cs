7c2b54bd050d369b98a65d4b5fc1179a331c9b81
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-10-14 23:08:00 -0300

    Adicionei dois campos: um para armazenar a mensagem em si, e o outro para guardar o nome do Sender

--- ZyronChatWebApp/Models/Messages.cs
+++ ZyronChatWebApp/Models/Messages.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
-
+using System.ComponentModel.DataAnnotations;
 using ZyronChatWebApp.Models;
 
 namespace ZyronChatWebApp.Models
 {
     public class Messages:IdentityUser
     {
-     
+        [Key]
+        public int Id { get; set; }
+
+        public string Sender { get; set; }
+        public string Message { get; set; }
     }
 }
