d0d473f7ed63603afdf820a3858e471aa4728e0d
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-10-14 22:05:31 -0300

    Criei o model de ChatMessages.  Esse model é o responsável por armazenar as informações de um chat entre dois usuários.

---
+++ ZyronChatWebApp/Models/ChatMessages.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZyronChatWebApp.Models
+{
+    public class ChatMessages 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int IdUserSender { get; set; }
+        public UserModelCustom UserSender { get; set; }
+
+        public int IdUserReceiver { get; set; }
+        public UserModelCustom UserReceiver { get; set; }
+
+            
+    }
+}
