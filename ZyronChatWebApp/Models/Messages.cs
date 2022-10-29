e0752669427007bc2fb179fa896e9c58705a92e0
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-10-28 21:16:01 -0300

    Alterei o tipo do id e criei os campos necessário para referênciar o model ChatMessages, sendo possível dessa forma criar uma relação many to one .

--- ZyronChatWebApp/Models/Messages.cs
+++ ZyronChatWebApp/Models/Messages.cs
@@ -7,12 +7,18 @@ namespace ZyronChatWebApp.Models
     public class Messages
     {
         [Key]
-        public int Id { get; set; }
+        public string Id { get; set; }
 
         public string Sender { get; set; }
 
         public DateTime DateSended { get; set; }
         public TimeSpan TimeSended { get; set; }
         public string Message { get; set; }
+
+
+        
+        public string ChatMessagesId { get; set; }
+        public ChatMessages ChatMessages { get; set; }
+
     }
 }
