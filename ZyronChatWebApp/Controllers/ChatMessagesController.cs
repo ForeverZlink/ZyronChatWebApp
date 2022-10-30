53410b196316ddf024b9dea84d9521449cab2914
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-10-29 22:23:30 -0300

    Foi criado um método para salvar as messages entre o usuário que está logado e o outro que irá receber.
    Após serem buscados os usuários, é definido um rotulo para ser acessado via goto, para que o controle do código volte para esse ponto.  Esse goto é necessário para caso não seja possivel encontrar um Chat relacionado ao dois usuários. Caso o chat seja null, é chamado o método de criação de chat ( o responsável por criar uma entidade do tipo ChatMessage) e é passado o id do usuário que receberá a mensagem.
    
    Com a entidade criada, é usado o goto para com o controle do código para o ponto                     SearchCha, pois como agora foi criado a entidade do tipo ChatMessages anteriormente, o programa será capaz de achar o objeto no banco de dados e salvar a mensagem.

--- ZyronChatWebApp/Controllers/ChatMessagesController.cs
+++ ZyronChatWebApp/Controllers/ChatMessagesController.cs
@@ -31,18 +31,47 @@ namespace ZyronChatWebApp.Controllers
                     await this.Context.SaveChangesAsync();
 
                 }
-            
-        }
+                
+            }
             return NotFound();
         }
+        public bool SaveMessagesChatBetweenTwoUsers(string UserToSendAMessageIdentification,string message)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                
+                string usernameActualUser = User.Identity.Name;
+                var user = this.Context.Users.FirstOrDefault(x => x.UserName == usernameActualUser );
+                var userToSend = this.Context.Users.FirstOrDefault(x => x.UserName == UserToSendAMessageIdentification);
+                if (userToSend != null || user!=null)
+                {
+                    SearchChat:
+                        var Chat = this.Context.ChatMessages.FirstOrDefault(
+                            x => x.IdUserReceiver == user.Id && x.IdUserSender == userToSend.Id ||
+                                x.IdUserSender == user.Id && x.IdUserReceiver == userToSend.Id
+                            );
+                    
+                        if (Chat != null)
+                        {
+                            DateTime DatetimeTodaySended = DateTime.UtcNow.Date;
+                            TimeSpan TimeMessageSended = DateTime.UtcNow.TimeOfDay;
+                            var MessageSaved = new Messages() {Sender=user.UserName ,Message = message, TimeSended = TimeMessageSended, DateSended = DatetimeTodaySended };
+                        
+                            Chat.MessagesList.Add(MessageSaved);
+                            this.Context.Update(Chat);
+                            this.Context.SaveChanges();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public async Task<IActionResult> ChatWithASpecificUser(string UserToSendAMessageIdentification)
-        {
-            return View();
+
+                            return true;
+                        }
+                        else
+                        {
+                            this.CreateNewChat(userToSend.Id);
+                            goto SearchChat;
+                        }
+                }
+            }
+            return false;
         }
     }
 }
