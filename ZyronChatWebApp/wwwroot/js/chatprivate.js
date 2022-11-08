d0252ec91ba59190400c7d0bba9dd922610e2f24
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-11-08 20:06:06 -0300

    Alterei o nome do arquivo javascript responsável por fazer a conexão de hubs do signalr. Além disso, também alterei  o nome do método do hub que é chamado.

--- ZyronChatWebApp/wwwroot/js/chatprivate.js
+++ ZyronChatWebApp/wwwroot/js/chatprivate.js
@@ -1,16 +1,17 @@
-﻿
+﻿"use strict";
+
 var connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
 
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
+connection.on("ReceiveMessagePrivate", function ( message) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
     // We can assign user-supplied strings to an element's textContent because it
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
+    li.textContent = message;
 });
 
 connection.start().then(function () {
@@ -22,7 +23,7 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessageToAll", user, message).catch(function (err) {
+    connection.invoke("SendMessagePrivate", user, message).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
