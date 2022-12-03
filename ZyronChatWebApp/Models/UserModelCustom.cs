b951b6fafa2232baf731cb7e8b2ed1579e9a5ea0
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-12-03 18:35:25 -0300

    Como está sendo utilizado o mode UserPublic para fazer referências, removi os campos do model UserModelCustom, pois não serão usados.

--- ZyronChatWebApp/Models/UserModelCustom.cs
+++ ZyronChatWebApp/Models/UserModelCustom.cs
@@ -6,12 +6,7 @@ namespace ZyronChatWebApp.Models
 {
     public class UserModelCustom:IdentityUser
     {
-         public UserScheduleListOfContacts UserScheduleListOfContacts { get; set; }
-
-        [InverseProperty("UserSender")]
-        public ICollection<ChatMessages> UsersSender { get; set; }
-
-        [InverseProperty("UserReceiver")]
-        public ICollection<ChatMessages> UserReceiver { get; set; }
+    
     }
+    
 }
