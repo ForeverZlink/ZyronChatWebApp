8de95f5f8d913b7c884955a295b3199420019053
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-12-04 18:00:24 -0300

    Alterado os nomes dos paramêtros. O campo UsernameOfIdentification do model ContactInformations agora recebe o IdPublic.

--- ZyronChatWebApp/ModelsLogicActions/UserScheduleListOfContacts.cs
+++ ZyronChatWebApp/ModelsLogicActions/UserScheduleListOfContacts.cs
@@ -15,17 +15,17 @@ namespace ZyronChatWebApp.Logics
             this.Context = userContext;
         }
 
-        public bool AddNewContact(string IdUser, string IdOfContact, string Surname)
+        public bool AddNewContact(string IdPublicUser, string IdOfContactPublic, string Surname)
         {
             //Seach in database the object create when the user was created in the first time. 
             //Here its possible find because when the user was created a new UserScheduleListOfContacts object receive a UserId
             //This Userid its the prove of relationship among both entitys
             //Not its possible exists two UserScheduleListOfContacts with the same UserId
-            if(IdOfContact ==null && IdUser==null)
+            if(IdOfContactPublic ==null && IdPublicUser == null)
             {
                 return false;
             }
-            var UserScheduleListOfContactsInstance = this.Context.UserScheduleListOfContacts.FirstOrDefault(x => x.UserId == IdUser);
+            var UserScheduleListOfContactsInstance = this.Context.UserScheduleListOfContacts.FirstOrDefault(x => x.UserId == IdPublicUser);
             if (UserScheduleListOfContactsInstance == null)
             {
                 return false;
@@ -33,13 +33,13 @@ namespace ZyronChatWebApp.Logics
 
             var ContactInformation = this.Context.ContactInformations.FirstOrDefault(
                 x => x.IdUserScheduleListOfContacts == UserScheduleListOfContactsInstance.Id
-                && x.UsernameOfIdentification == IdOfContact);
+                && x.UsernameOfIdentification == IdOfContactPublic);
 
             if (ContactInformation == null)
             {
                 var ContactInfo = new ContactInformations()
                 {
-                    UsernameOfIdentification = IdOfContact,
+                    UsernameOfIdentification = IdOfContactPublic,
                     Surname = Surname,
                     IdUserScheduleListOfContacts = UserScheduleListOfContactsInstance.Id
                 };
