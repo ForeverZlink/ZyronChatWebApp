ec332669a7b8427100612a2ed5ecf0b5b85eba39
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-12-31 20:38:06 -0300

    Criei uma query para criar um novo objeto que é relacionado com outro objeto. Essa query usa dos valores de um objeto já existente para apenas criar objetos (de outra tabela e que ainda não exista alguma referência nela).

---
+++ SQLQuery1.sql
@@ -0,0 +1,38 @@
+﻿declare @IdsUserPublicWithNotNotificationModels table (IdUserPublic varchar(100))
+declare @Counter integer =1
+
+
+Insert into @IdsUserPublicWithNotNotificationModels Select   UserPublic.IdPublic
+		from UserPublic as UserPublic
+		left join  Notifications as Nofications
+			on UserPublic.IdPublic = Nofications.IdUserCreatorOfNotification
+		where Nofications.IdUserCreatorOfNotification is NULL ;
+
+declare @QuantityOfLoops integer = (Select Count(IdUserPublic) from @IdsUserPublicWithNotNotificationModels);
+
+while 1=1
+	begin
+		if @Counter >@QuantityOfLoops
+			begin
+				break
+			end
+		
+		insert into Notifications  (Id,IdUserCreatorOfNotification)
+		values(LOWER(newid()),
+			 (Select top 1 IdUserPublic from @IdsUserPublicWithNotNotificationModels
+				) ) ;
+	
+	With RowToDelete as (
+		Select top 1 IdUserPublic from @IdsUserPublicWithNotNotificationModels)
+	
+	delete from RowToDelete;
+	
+	
+		
+
+	set @Counter +=1;
+	end;
+
+Select * from Notifications;
+
+ 
